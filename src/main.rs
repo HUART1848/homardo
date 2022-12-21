8d8dee90849b349b3c6af395ed91788ce2c7501c
Hugo Huart <hugo.huart@heig-vd.ch>
2022-12-22 00:19:53 +0100

    u8 to u32

--- src/main.rs
+++ src/main.rs
@@ -4,15 +4,22 @@ const N_NUMBERS: usize = 6;
 
 #[derive(Debug)]
 struct Compte {
-    numbers: Vec<u8>,
+    numbers: Vec<u32>,
+}
+
+enum Operation {
+    Add,
+    Sub,
+    Mul,
+    Div,
 }
 
 impl Compte {
     fn new() -> Self {
-        let mut allowed_numbers: Vec<u8> = vec![
+        let mut allowed_numbers: Vec<u32> = vec![
             1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10, 25, 50, 75, 100,
         ];
-        let mut numbers: Vec<u8> = Vec::new();
+        let mut numbers: Vec<u32> = Vec::new();
         numbers.reserve(N_NUMBERS);
 
         for _ in 0..N_NUMBERS {
@@ -24,8 +31,4 @@ impl Compte {
     }
 }
 
-fn main() {
-    for _ in 0..100 {
-        println!("{:?}", Compte::new())
-    }
-}
+fn main() {}
